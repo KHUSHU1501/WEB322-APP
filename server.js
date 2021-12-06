9bace33429d7ac619f17842c546f4d18b7ae2f4b
Khushwant Singh Rao <technological.mess@gmail.com>
2021-12-05 20:47:52 -0500

    Assign 6

--- server.js
+++ server.js
@@ -18,11 +18,20 @@ const fs = require("fs");
 const multer = require("multer");
 const exphbs = require('express-handlebars');
 const app = express();
-
-
+const dataServiceAuth = require("./data-service-auth");
+const clientSessions = require("client-sessions");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
+  
 app.engine('.hbs', exphbs({ 
     extname: '.hbs',
     defaultLayout: "main",
@@ -53,31 +62,37 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs');
 
-// multer requires a few options to be setup to store files with file extensions
-// by default it won't store extensions for security reasons
-const storage = multer.diskStorage({
-    destination: "./public/images/uploaded",
-    filename: function (req, file, cb) {
-      // we write the filename as the current date down to the millisecond
-      // in a large web service this would possibly cause a problem if two people
-      // uploaded an image at the exact same time. A better way would be to use GUID's for filenames.
-      // this is a simple example.
-      cb(null, Date.now() + path.extname(file.originalname));
-    }
-  });
-  
-  // tell multer to use the diskStorage function for naming files instead of the default.
-  const upload = multer({ storage: storage });
-
+const upload = multer({ storage: storage });
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(clientSessions({
+    cookieName: "session", 
+    secret: "this_is_some_super_secret_string_for_web322_assignment6", 
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60
+}));
 
 app.use(function(req,res,next){
     let route = req.baseUrl + req.path;
     app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
     next();
 });
+
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+
+
+function ensureLogin(req, res, next) {
+    if (!req.session.user) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+}
 
 app.get("/", (req,res) => {
     res.render("home");
@@ -87,29 +102,27 @@ app.get("/about", (req,res) => {
     res.render("about");
 });
 
-app.get("/images/add", (req,res) => {
+app.get("/images/add", ensureLogin,(req,res) => {
     res.render("addImage");
 });
 
-app.get("/employees/add", (req,res) => {
+app.get("/employees/add", ensureLogin,(req,res) => {
 
     data.getDepartments().then((data)=>{
         res.render("addEmployee", {departments: data});
      }).catch((err) => {
        // set department list to empty array
        res.render("addEmployee", {departments: [] });
-    });
-  
-  
+    }); 
   });
 
-app.get("/images", (req,res) => {
+app.get("/images", ensureLogin,(req,res) => {
     fs.readdir("./public/images/uploaded", function(err, items) {
         res.render("images",{images:items});
     });
 });
 
-app.get("/employees", (req, res) => {
+app.get("/employees", ensureLogin,(req, res) => {
     
    if (req.query.status) {
         data.getEmployeesByStatus(req.query.status).then((data) => {
@@ -138,7 +151,7 @@ app.get("/employees", (req, res) => {
     }
 });
 
-app.get("/employee/:empNum", (req, res) => {
+app.get("/employee/:empNum", ensureLogin,(req, res) => {
 
     // initialize an empty object to store the values
     let viewData = {};
@@ -180,7 +193,7 @@ app.get("/employee/:empNum", (req, res) => {
 
 
 
-app.get("/departments", (req,res) => {
+app.get("/departments", ensureLogin,(req,res) => {
     data.getDepartments().then((data)=>{
         res.render("departments", (data.length > 0) ? {departments:data} : { message: "no results" });
     }).catch((err) => {
@@ -189,7 +202,7 @@ app.get("/departments", (req,res) => {
 });
 
 
-app.post("/employees/add", (req, res) => {
+app.post("/employees/add", ensureLogin,(req, res) => {
     data.addEmployee(req.body).then(()=>{
       res.redirect("/employees"); 
     }).catch((err)=>{
@@ -197,12 +210,12 @@ app.post("/employees/add", (req, res) => {
       });
   });
 
-app.post("/images/add", upload.single("imageFile"), (req,res) =>{
+app.post("/images/add", ensureLogin,upload.single("imageFile"), (req,res) =>{
     res.redirect("/images");
 });
 
 
-app.post("/employee/update", (req, res) => {
+app.post("/employee/update", ensureLogin,(req, res) => {
     data.updateEmployee(req.body).then(()=>{
     res.redirect("/employees");
   }).catch((err)=>{
@@ -211,11 +224,11 @@ app.post("/employee/update", (req, res) => {
   
 });
 
-app.get("/departments/add", (req,res) => {
+app.get("/departments/add", ensureLogin,(req,res) => {
     res.render("addDepartment");
   });
   
-  app.post("/departments/add", (req, res) => {
+app.post("/departments/add", ensureLogin,(req, res) => {
     data.addDepartment(req.body).then(()=>{
       res.redirect("/departments");
     }).catch((err)=>{
@@ -224,7 +237,7 @@ app.get("/departments/add", (req,res) => {
   });
   
 
-  app.post("/department/update", (req, res) => {
+  app.post("/department/update", ensureLogin,(req, res) => {
       data.updateDepartment(req.body).then(()=>{
       res.redirect("/departments");
     }).catch((err)=>{
@@ -233,8 +246,7 @@ app.get("/departments/add", (req,res) => {
     
   });
   
-  app.get("/department/:departmentId", (req, res) => {
-  
+  app.get("/department/:departmentId", ensureLogin,(req, res) => {  
     data.getDepartmentById(req.params.departmentId).then((data) => {
         if(data){
             res.render("department", { data: data });
@@ -245,10 +257,9 @@ app.get("/departments/add", (req,res) => {
     }).catch((err) => {
       res.status(404).send("Department Not Found");
     });
-  
   });
 
-  app.get("/employees/delete/:empNum", (req,res)=>{
+  app.get("/employees/delete/:empNum", ensureLogin,(req,res)=>{
     data.deleteEmployeeByNum(req.params.empNum).then(()=>{
       res.redirect("/employees");
     }).catch((err)=>{
@@ -256,7 +267,7 @@ app.get("/departments/add", (req,res) => {
     });
   });
 
-  app.get("/departments/delete/:depId", (req,res)=>{
+  app.get("/departments/delete/:depId", ensureLogin,(req,res)=>{
     data.deleteDepartmentById(req.params.depId).then(()=>{
       res.redirect("/departments");
     }).catch((err)=>{
@@ -264,15 +275,68 @@ app.get("/departments/add", (req,res) => {
     });
   });
 
+
+
+ 
+
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+   });
+     
+
+app.get("/login", function(req, res) {
+    res.render('login');
+});
+
+app.get("/register", function(req, res) { 
+    res.render('register');
+});
+
+app.post("/register", function(req, res) {
+    dataServiceAuth.registerUser(req.body)
+    .then(() => res.render('register', { successMsg: "User created!"}))
+    .catch((err) => res.render('register', { errorMsg: err, userName: req.body.userName }));
+});
+
+app.post("/login", function(req, res) {
+    req.body.userAgent = req.get('User-Agent');
+
+    dataServiceAuth.checkUser(req.body)
+    .then(function(user) { 
+        req.session.user = {
+            userName: user.userName,
+            email: user.email,
+            loginHistory: user.loginHistory
+        }
+
+        res.redirect('/employees');
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.render('login', { errorMsg: err, userName: req.body.userName });
+    });
+});
+
+app.get("/logout", function(req, res) {
+    req.session.reset();
+    res.redirect('/');
+});
+
+app.get("/userHistory", ensureLogin, function (req, res) {
+    res.render('userHistory');
+}); 
+
 app.use((req, res) => {
     res.status(404).send("Page Not Found");
   });
 
-data.initialize().then(function(){
-    app.listen(HTTP_PORT, function(){
-        console.log("app listening on: " + HTTP_PORT)
-    });
-
+data.initialize()
+.then(dataServiceAuth.initialize)
+.then(function(){
+ app.listen(HTTP_PORT, function(){
+ console.log("app listening on: " + HTTP_PORT)
+ });
 }).catch(function(err){
-    console.log("unable to start server: " + err);
+ console.log("unable to start server: " + err);
 });
